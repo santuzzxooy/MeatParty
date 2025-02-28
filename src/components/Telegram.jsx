882e8f039e4fb64943e0e2946081b728213f1ae7
santuzzxooy <santiagorochav@gmail.com>
2025-02-28 00:58:13 -0500

    arreglo de header

---
+++ src/components/Telegram.jsx
@@ -0,0 +1,20 @@
+import './styles/Telegram.css'
+
+const Telegram = () => {
+    return (
+        <div>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous" />
+
+            <div class="tooltip-container">
+
+                <a href="https://t.me/hydrogarden" class="telegram-icon-float" target="_blank">
+                    <i class="fab fa-telegram my-float">
+                        <span class="tooltip">Unete a nuestro canal de Telegram</span>
+                    </i>
+                </a>
+                </div>
+                </div>
+                )
+}
+
+export default Telegram
